09aaf0f7c64f58170237dbf69a157d0519e719d0
majster-pl <waliczek.szymon@gmail.com>
2022-03-08 00:46:58 +0000

    Add animation on hover to skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -5,12 +5,22 @@ import Jello from "react-reveal/Jello";
 
 const Skill = ({ index, url, title }) => {
   const [visable, setVisable] = useState(false);
+  const [mousHover, setMouseHover] = useState(false);
 
   const randomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return (Math.floor(Math.random() * (max - min + 1)) + min) * 300;
   } 
+
+  const trigerAnimation = () => {
+      setMouseHover(true);
+      setVisable(!visable);
+  }
+
+  const stopAnimation = () => {
+      setMouseHover(false);
+  }
 
   return (
     <VisibilitySensor
@@ -19,9 +29,14 @@ const Skill = ({ index, url, title }) => {
       }}
     >
       <Col key={index}>
-        <Jello spy={visable} delay={randomNumber(1,11)}>
+        <Jello spy={visable} delay={mousHover ? 0 : randomNumber(1, 11)}>
           <Row className="m-2 p-2 skill-hover-zoom">
-            <Image className="mb-2" src={url}></Image>
+            <Image
+              className="mb-2"
+              src={url}
+              onMouseEnter={trigerAnimation}
+              onMouseLeave={stopAnimation}
+            ></Image>
             <p className="fs-5 fw-normal">{title}</p>
           </Row>
         </Jello>
