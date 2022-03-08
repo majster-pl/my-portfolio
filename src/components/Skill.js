a61b01cd1a7077a7601e7b1da2dc4287710bf956
majster-pl <waliczek.szymon@gmail.com>
2022-03-08 00:18:20 +0000

    Add animation with random timer to skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
 import { Col, Row, Image } from "react-bootstrap";
+import VisibilitySensor from "react-visibility-sensor";
+import Jello from "react-reveal/Jello";
 
 const Skill = ({ index, url, title }) => {
+  const [visable, setVisable] = useState(false);
+
+  const randomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return (Math.floor(Math.random() * (max - min + 1)) + min) * 300;
+  } 
+
   return (
-    <Col className="" key={index}>
-      <Row className="m-2 p-2 skill-hover-zoom">
-        <Image className="mb-2" src={url}></Image>
-        <p className="fs-5 fw-normal">{title}</p>
-      </Row>
-    </Col>
+    <VisibilitySensor
+      onChange={(isVisible) => {
+        setVisable(isVisible);
+      }}
+    >
+      <Col key={index}>
+        <Jello spy={visable} delay={randomNumber(1,11)}>
+          <Row className="m-2 p-2 skill-hover-zoom">
+            <Image className="mb-2" src={url}></Image>
+            <p className="fs-5 fw-normal">{title}</p>
+          </Row>
+        </Jello>
+      </Col>
+    </VisibilitySensor>
   );
 };
 
