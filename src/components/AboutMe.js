14d08530c9911f41dbe2f7acd8e71845dc618204
majster-pl <waliczek.szymon@gmail.com>
2022-03-11 22:02:29 +0000

    Update about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,91 +1,63 @@
-import { Link } from 'react-scroll'
-import { Fade } from 'react-reveal';
-import { Container } from 'react-bootstrap';
+import { Link } from "react-scroll";
+import { Fade } from "react-reveal";
+import { Container } from "react-bootstrap";
 
 const AboutMe = ({ id }) => {
-    return (
-      <div id={id} className="w-100 m-0 p-0">
-        <Container className="pb-5 unselectable">
-          <Fade top cascade>
-            <p className="fs-1 text-gray fw-bold">About Me</p>
+  return (
+    <div id={id} className="w-100 m-0 p-0">
+      <Container className="pb-5 unselectable">
+        <Fade top cascade>
+          <p className="fs-1 text-gray fw-bold">About Me</p>
+        </Fade>
+
+        <div className="fs-3 fw-light">
+          <Fade bottom>
+            <p className="text-justify">
+              My name is Szymon, I'm self taught freelance web developer based
+              in Bristol, UK. My coding journey started of as a hobby back in
+              2011 by developing Linux desktop applications using GTK and
+              Python. One of my desktop applications was{" "}
+              <a
+                rel="noreferrer"
+                href="http://linuxg.net/ubuntu-touch-manager-how-to-install-and-test-ubuntu-touch-manager-on-ubuntu-13-04-12-10-12-04/"
+                target="_blank"
+              >
+                Ubuntu Touch Manager.
+              </a>
+            </p>
+            <p className="text-justify">
+              Then I got involved in Open Source projects developing mobile
+              application for Linux Mobile OS -{" "}
+              <a
+                rel="noreferrer"
+                href="https://en.wikipedia.org/wiki/Ubuntu_Touch"
+                target="_blank"
+              >
+                Ubuntu Touch
+              </a>{" "}
+              but most of my career I'm focused on developing websites using
+              React, Laravel, HTML5, JavaScript, CSS3. Below you can see few of{" "}
+              <Link
+                className="text-link"
+                to="projects"
+                spy={true}
+                offset={-100}
+              >
+                My Projects
+              </Link>{" "}
+              and live pages to them.
+            </p>
+            <p className="text-justify">
+              I love new technologies and gadgets, I'm always looking for a ways
+              to automate things to make a life easier. When possible I spend my
+              free time to teach my 7 years old son electronic and how to code
+              by experimenting with MicroBit and Raspberry Pi.
+            </p>
           </Fade>
+        </div>
+      </Container>
+    </div>
+  );
+};
 
-          <div className="fs-3 fw-light">
-            <Fade bottom>
-              <p className="text-justify">
-                My name is Szymon, I’m freelance full stack web developer based
-                in Bristol UK. I have begun my coding journey back in 2011 and
-                it started out as a hobby by developing Linux desktop
-                applications using GTK for a front-end and Python in the
-                back-end. One of my desktop aplications was{" "}
-                <a
-                  rel="noreferrer"
-                  href="http://linuxg.net/ubuntu-touch-manager-how-to-install-and-test-ubuntu-touch-manager-on-ubuntu-13-04-12-10-12-04/"
-                  target="_blank"
-                >
-                  Ubuntu Touch Manager
-                </a>
-                .
-              </p>
-              <p className="text-justify">
-                After adventure with desktop applications I got involved in open
-                source projects developing mobile application for newly (back
-                then) created operating system{" "}
-                <a
-                  rel="noreferrer"
-                  href="https://en.wikipedia.org/wiki/Ubuntu_Touch"
-                  target="_blank"
-                >
-                  Ubuntu Touch
-                </a>{" "}
-                by{" "}
-                <a rel="noreferrer" href="https://ubuntu.com/" target="_blank">
-                  Cannonical
-                </a>
-                . I was developing applications which you can download from{" "}
-                <a
-                  rel="noreferrer"
-                  href="https://open-store.io/?sort=relevance&amp;search=author%3ASzymon%20Waliczek"
-                  target="_blank"
-                >
-                  OpenStore
-                </a>{" "}
-                and some of them you can see in{" "}
-                <Link
-                  className="text-link"
-                  to="projects"
-                  spy={true}
-                  offset={-100}
-                >
-                  My Projects
-                </Link>
-                .
-              </p>
-              <p className="text-justify">
-                Most of my cariera I've focused on developing web applications
-                using React, Laravel, HTML5, JavaScript, jQuery, CSS3 and other
-                great frameworks and languages. Below you can see few of{" "}
-                <Link
-                  className="text-link"
-                  to="projects"
-                  spy={true}
-                  offset={-100}
-                >
-                  My Projects
-                </Link>{" "}
-                and live pages to them.
-              </p>
-              <p className="text-justify">
-                I love gadgets and learn about new technologies, I always
-                looking for a ways to automate things to make a life easier.
-                When possible I spent my free time to catch up with my family
-                and friends.
-              </p>
-            </Fade>
-          </div>
-        </Container>
-      </div>
-    );
-}
-
-export default AboutMe
+export default AboutMe;
