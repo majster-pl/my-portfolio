2a6606a8f48e2709f3e8ae1ad64673ec4af5dfe7
majster-pl <waliczek.szymon@gmail.com>
2022-05-05 12:19:56 +0100

    Remove animation on hover on skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,28 +1,12 @@
-import { useState } from "react";
 import { Col, Row, Image } from "react-bootstrap";
-import Jump from "react-reveal/Jump";
 
 const Skill = ({ url, title }) => {
-  const [startAnim, setStartAnim] = useState(false);
-
-  const trigerAnimation = () => {
-    setStartAnim(!startAnim);
-  };
-
-
   return (
     <Col>
-      <Jump spy={startAnim}>
-        <Row className="m-2 p-2 skill-hover-Jump">
-          <Image
-            className="mb-2"
-            src={url}
-            onMouseEnter={trigerAnimation}
-            onTouchStart={trigerAnimation}
-          ></Image>
-          <p className="mx-0 px-0 fs-6 fw-normal">{title}</p>
-        </Row>
-      </Jump>
+      <Row className="m-2 p-2 skill-hover-Jump">
+        <Image className="mb-2" src={url}></Image>
+        <p className="mx-0 px-0 fs-6 fw-normal">{title}</p>
+      </Row>
     </Col>
   );
 };
